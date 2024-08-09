fb8f34df5ac4bd892004a86ce839f6ee05d55ccb
Artyom <rzdartem14@gmail.com>
2024-08-09 03:13:17 +0300

    Изменение формулы для расчёта скорости
    Для расчёта была выбрана более точная формула. Также изменился стиль
    кода (стиль инициализации, переименование переменных).

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,23 +1,69 @@
 #include "MS5611.h"
 
-namespace {
-constexpr float mbar2mmHg = 0.7500616;
-constexpr float mbar2Pa = 100.0;
+MS5611 STATIC{ 0x76 };  // поменять адреса местами, если неправильно
+MS5611 TOTAL{ 0x77 };
 
-constexpr float gravAcceleration = 9.81;
+/* \varepsilon (ε) */
+float compressibilityCorrection;  // поправка на сжимаемость потока
+
+namespace {
+/* \sigma (ς) */
+constexpr float kCorrectionFactor{ 1. };  // поправочный коэффициент, необходимо поменять после опыта
+
+constexpr float kMbar2mmHg{ 0.7500616 };
+constexpr float kMbar2Pa{ 100. };
+
+constexpr float kGravAcceleration{ 9.81 };
+
+inline void checkRead() {
+  int res{ STATIC.read() };
+  if (res != MS5611_READ_OK) {
+    Serial.print(F("0x76 Read err: "));
+    Serial.println(res);
+  }
+
+  res = TOTAL.read();
+  if (res != MS5611_READ_OK) {
+    Serial.print(F("0x77 Read err: "));
+    Serial.println(res);
+  }
+}
 
 /*! \brief
  *  \param pressure Pressure in mmHg
  *  \param temperature Temperature in C
- *  \return Returns the density value in SI
+ *  \return Returns the density value in SI (kg/m^3)
  */
 inline float getDensity(float pressure, float temperature) {
-  return 0.125 * (1 + 0.0013 * (pressure - 760) - 0.0036 * (temperature - 15)) * gravAcceleration;
+  return 0.125 * (1 + 0.0013 * (pressure - 760) - 0.0036 * (temperature - 15)) * kGravAcceleration;
 }
-}  // end of nameless namespace
 
-MS5611 STATIC(0x76);
-MS5611 TOTAL(0x77);
+/*! \brief
+ *  \param staticPressure Pressure in mbar
+ *  \return Returns the differential pressure in Pa
+ */
+inline float getdiffPressure(float staticPressure) {
+  return (TOTAL.getPressure() - staticPressure) * kMbar2Pa;
+}
+/*! \brief
+ *  \param staticPressure Pressure in mbar
+ *  \return Returns the differential pressure in Pa
+ */
+inline float getAirspeed(float diffPressure, float density, float compressibilityCorrection) {
+  /* V = sqrt( 2 * ς * (p_1 - p_2) / [ρ * (1 + ε)]) */
+  return M_SQRT2 * sqrtf(kCorrectionFactor * diffPressure / (density * (1 + compressibilityCorrection)));
+}
+
+/*! \brief
+ *  \param airspeed airspeed
+ *  \return Returns the compressibilityCorrection
+ */
+inline float getCompressibilityCorrection(float airspeed) {
+  /*  ε = (M^2)/4 = (V / 330)^2 / 4 = V^2 / 435600 */
+  return airspeed * airspeed / 435600;
+}
+
+}  // end of nameless namespace
 
 void setup() {
   Serial.begin(115200);
@@ -25,15 +71,15 @@ void setup() {
     ;
 
   Wire.begin();
-  bool one_begin = STATIC.begin();
-  bool two_begin = TOTAL.begin();
-  if (one_begin == true && two_begin == true) {
+  bool oneBegin{ STATIC.begin() };
+  bool twoBegin{ TOTAL.begin() };
+  if (oneBegin == true and twoBegin == true) {
     Serial.println(F("Two MS5611 are found."));
   } else {
-    if (!one_begin) {
+    if (!oneBegin) {
       Serial.println(F("0x76 not found. Halt."));
     }
-    if (!two_begin) {
+    if (!twoBegin) {
       Serial.println(F("0x77 not found. Halt."));
     }
 
@@ -47,31 +93,18 @@ void setup() {
 
 
 void loop() {
-  int res = STATIC.read();
-  if (res != MS5611_READ_OK) {
-    Serial.print(F("0x76 Read err: "));
-    Serial.println(res);
-  }
+  checkRead();
 
-  res = TOTAL.read();
-  if (res != MS5611_READ_OK) {
-    Serial.print(F("0x77 Read err: "));
-    Serial.println(res);
-  }
+  float staticPressure{ STATIC.getPressure() };        // in mbar
+  float staticTemperature{ STATIC.getTemperature() };  // in °C
 
-  float staticPressure = STATIC.getPressure();                              // in mbar
-  float staticTemperature = STATIC.getTemperature();                        // in C
+  float density{ getDensity(staticPressure, staticTemperature * kMbar2mmHg) };  // in kg/m^3
 
-  float diffPressure = (TOTAL.getPressure() - staticPressure) * mbar2Pa;    // in Pa
-  float avgTemperature = (staticTemperature + TOTAL.getTemperature()) / 2;  // in C
-  staticPressure *= mbar2mmHg;                                              // in mmHg
+  float diffPressure{ getdiffPressure(staticPressure) };  // in Pa
 
-  float airspeed = M_SQRT2 * sqrtf(diffPressure / getDensity(staticPressure, staticTemperature));
+  float airspeed{ getAirspeed(diffPressure, density, compressibilityCorrection) };
 
-  Serial.print(F("$"));
-  Serial.print(airspeed, 2);
-  Serial.print(F(","));
-  Serial.print(avgTemperature, 2);
-  Serial.print(F(";"));
-  delay(10);
+  Serial.println(airspeed, 2);
+
+  compressibilityCorrection = getCompressibilityCorrection(airspeed);
 }
